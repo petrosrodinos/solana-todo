56e59be48c85361e185f1db67c8938127795c29e
Petros <petros.rodinos@yahoo.com>
2025-02-10 12:14:18 +0200

    init next/anchor

---
+++ app/address/page.tsx
@@ -0,0 +1,12 @@
+import WalletAddress from "@/app/components/WalletAddress";
+import React from "react";
+
+const Address = () => {
+  return (
+    <div>
+      <WalletAddress />
+    </div>
+  );
+};
+
+export default Address;
---
+++ app/constants/index.ts
@@ -0,0 +1,3 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const TODO_PROGRAM_PUBKEY = new PublicKey("6fAn2AkGkrmFHKDLgRXJEbNvDg84WpyLgKgwnS1it8xy");
