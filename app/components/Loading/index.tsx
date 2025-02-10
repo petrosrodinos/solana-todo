d8999f5de5e549a9b736ab0e844273f447b57b23
Petros <petros.rodinos@yahoo.com>
2025-02-10 16:23:34 +0200

    better spinner

--- app/components/Loading/index.tsx
+++ app/components/Loading/index.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
 
 const Loading: FC<any> = ({ loading, children }) => {
-  if (loading) return <p>Loading...</p>;
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center">
+        <div className="border-4 border-t-4 border-gray-200 border-t-blue-500 rounded-full w-10 h-10 animate-spin"></div>
+      </div>
+    );
+  }
 
   return <>{children}</>;
 };
